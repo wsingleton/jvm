358be4c37c877fb9e79629ca3dc356f8a651cdce
Wezley <wezley.singleton@gmail.com>
2021-07-07 14:49:48 -0400

    make jvm executable upon install, and added script documentation

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Create application directories
 mkdir ~/.jvm
 mkdir ~/.jvm/bin
 mkdir ~/.jvm/current
 mkdir ~/.jvm/installed-versions
 mkdir ~/.jvm/tmp
 
+# Copy jvm file to created bin directory and make it an executable
 cp ./jvm ~/.jvm/bin/jvm
+chmod +x ~/.jvm/bin/jvm
 
+# Add location of jvm executable and location of current Java version bin to PATH
 echo export PATH=\$HOME/.jvm/bin:\$HOME/.jvm/current/bin:\$PATH >> $HOME/.bashrc
 
