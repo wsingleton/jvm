9ee062011afbcf6a13e1dd14c64495e1e66bdc01
connellrobert <robert.lel.connell@gmail.com>
2021-07-08 11:42:10 -0500

    uninstall script complete

---
+++ uninstall.ps1
@@ -0,0 +1,14 @@
+# Delete jvm directories
+Remove-Item -Recurse -Force (Resolve-Path ~/.jvm)
+
+# Set new path environment variable for user and current terminal process
+$userPath = [Environment]::GetEnvironmentVariable("Path","User")
+$newPath = ""
+foreach ($path in $userPath.split(';')){
+    if($path -notlike "*.jvm*"){
+        $newPath += $path += ";"
+    }
+}
+[Environment]::SetEnvironmentVariable("Path","$newPath","User")
+[Environment]::SetEnvironmentVariable("Path","$newPath","Process")
+Write-Output "completed uninstallation"
